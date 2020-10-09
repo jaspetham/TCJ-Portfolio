fe6f57d2f99e4e3a4cd5270448f2426a5838b6c3
jaspertham <jaspertham98@gmail.com>
2020-10-10 00:27:49 +0800

    complete banner & navbar

--- src/Banner.js
+++ src/Banner.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import Typing from "react-typing-animation";
 import "./Banner.css";
 
 function Banner() {
@@ -9,11 +10,19 @@ function Banner() {
         <div className="banner-mask"></div>
         <div className="banner-bg"></div>
         <div className="banner-content  d-flex">
-          <Container className="my-auto">
+          <Container fluid className="my-auto">
             <Row>
-              <Col className="text-center text-white">
+              <Col className="text-center text-white p-0">
                 <p className="title mb-md-3">Welcome</p>
-                <h2 className="name mb-md-">I'm Tan Chang Jen</h2>
+                <h2 className="name mb-md-">
+                  <span>I'm </span>
+                  <Typing speed={100} loop={true} className="type-animation">
+                    Tan Chang Jen
+                    <Typing.Backspace count={20} speed={100} />
+                    a Game Developer
+                    <Typing.Backspace count={20} speed={100} />
+                  </Typing>
+                </h2>
                 <p className="location mb-md-">Based in KK</p>
                 <a href="#" className="btn custom-btn rounded mt-2">
                   Hire me
--- src/Header.js
+++ src/Header.js
@@ -1,41 +1,53 @@
 import React from "react";
-import { Container, Nav, Navbar, Image } from "react-bootstrap";
+import { Container, Nav, Navbar, Image, Row, Col } from "react-bootstrap";
 import "./Header.css";
 
 function Header() {
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid className="position-relative">
-        <Navbar.Brand href="#home">
-          <Image src="http://demo.harnishdesign.net/html/simone/images/logo-light.png" />
-        </Navbar.Brand>
+        <Row className="w-100">
+          <Col className="col-10 col-md-3">
+            <Navbar.Brand href="#home">
+              <Image src="http://demo.harnishdesign.net/html/simone/images/logo-light.png" />
+            </Navbar.Brand>
+          </Col>
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
-        <Navbar.Collapse className="justify-content-center">
-          <Nav className="nav-menu">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Nav.Link href="#home">Home</Nav.Link>
-              </li>
-              <li className="nav-item">
-                <Nav.Link href="#about">About</Nav.Link>
-              </li>
-              <li className="nav-item">
-                <Nav.Link href="#">What I do</Nav.Link>
-              </li>
-              <li className="nav-item">
-                <Nav.Link href="#">Resume</Nav.Link>
-              </li>
-              <li className="nav-item">
-                <Nav.Link href="#">Portfolio</Nav.Link>
-              </li>
-              <li className="nav-item">
-                <Nav.Link href="#">Contact</Nav.Link>
-              </li>
-            </ul>
-          </Nav>
-        </Navbar.Collapse>
+          <Navbar.Toggle>
+            <span className="icon-bar top-bar"> </span>
+            <span className="icon-bar middle-bar"> </span>
+            <span className="icon-bar bottom-bar"> </span>
+          </Navbar.Toggle>
+          <Col className="col col-md-6 p-0">
+            <Navbar.Collapse
+              className="justify-content-center"
+              id="navbarSupportedContent"
+            >
+              <Nav className="nav-menu">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                  </li>
+                  <li className="nav-item">
+                    <Nav.Link href="#about">About</Nav.Link>
+                  </li>
+                  <li className="nav-item">
+                    <Nav.Link href="#">What I do</Nav.Link>
+                  </li>
+                  <li className="nav-item">
+                    <Nav.Link href="#">Resume</Nav.Link>
+                  </li>
+                  <li className="nav-item">
+                    <Nav.Link href="#">Portfolio</Nav.Link>
+                  </li>
+                  <li className="nav-item">
+                    <Nav.Link href="#">Contact</Nav.Link>
+                  </li>
+                </ul>
+              </Nav>
+            </Navbar.Collapse>
+          </Col>
+        </Row>
       </Container>
     </Navbar>
   );
